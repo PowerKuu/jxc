131ab91dac129bb370de494cccbba0096edc3b05
PowerKuu <hkleven6@outlook.com>
2023-01-03 22:15:27 +0100

    upadate

--- run.bat
+++
@@ -1,1 +0,0 @@
-npx babel src --extensions ".tsx,.ts,.js" --out-file-extension .js --out-dir dist --copy-files 
--- src/cli/build.ts
+++ src/cli/build.ts
@@ -4,4 +4,8 @@ import { resolve } from "path"
 import { argv, cwd } from "process"
 import { build } from "../builder"
 
-build(resolve(cwd(), argv[1] ?? "./routes"), resolve(cwd(), argv[2] ?? "./dist"))
+const input = resolve(cwd(), argv[2] ?? "./routes")
+const output = resolve(cwd(), argv[3] ?? "./dist")
+
+console.log(`Starting build proccess input: ${input}, output: ${output}.`)
+build(input, output)
