c0b576830f573d13a9b4b1965787bba2cb70de47
PowerKuu <hkleven6@outlook.com>
2023-01-04 08:34:15 +0100

    update

--- src/cli/build.ts
+++ src/cli/build.ts
@@ -4,8 +4,8 @@ import { resolve } from "path"
 import { argv, cwd } from "process"
 import { build } from "../builder"
 
-const input = resolve(cwd(), argv[2] ?? "./routes")
-const output = resolve(cwd(), argv[3] ?? "./dist")
+const input = resolve(cwd(), argv[3] ?? "./routes")
+const output = resolve(cwd(), argv[4] ?? "./dist")
 
 console.log(`Starting build proccess input: ${input}, output: ${output}.`)
 build(input, output)
